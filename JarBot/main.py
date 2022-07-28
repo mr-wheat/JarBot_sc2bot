7db1018a7d6c636f46a4c11f329f447e26f1ff19
Cody Bollinger <cobol373@live.unc.edu>
2022-07-27 22:07:51 -0400

    Can defeat easy AI

--- JarBot/main.py
+++ JarBot/main.py
@@ -31,36 +31,43 @@ unit_type = {
 
 class JarBot(BotAI):
     """
-    Governs the Bot's behavior through the python_sc2 API.
+    Governs the Bot's behavior through the python_sc2 API. Currently Bot Employs a "swarm" tactic.
     """
     def __init__(self):
         self.attack_groups = set()
 
 
     async def on_step(self, iteration: int):
-        print(f"The iteration is {iteration}")
-        DESIRED_RAX_COUNT = round(1 + iteration/400) # Change later. 
-
+        desired_rax_count = round(1 + ((iteration * iteration) / (1100 + 100 * iteration)))
+        print(f"The iteration is {iteration}, desired rax count: {desired_rax_count}")
+        await self.distribute_workers() 
+        supply_remaining = self.supply_cap - self.supply_used
+        
 
         if self.townhalls:
             # thus we have command center
             command_center = self.townhalls.random
             rax_placement_posistion = None
 
+            for rax in self.structures(unit_type["rax"]).ready.idle:
+                if self.supply_cap != 200: # max supply
+                    if not self.can_afford(unit_type["marine"]):
+                        break
+                    rax.train(unit_type["marine"])
+
             # Train workers logic
-            if command_center.is_idle and self.can_afford(UnitTypeId.SCV):
-                if self.units(unit_type["scv"]).amount < 16 * self.townhalls.amount:
-                    command_center.train(unit_type["scv"])
+            if command_center.is_idle and self.can_afford(UnitTypeId.SCV) and self.units(unit_type["scv"]).amount <= 16 * self.townhalls.amount:
+                command_center.train(unit_type["scv"])
                 
 
             # Build Logic: First Supply Depot. One Supply Depot is required to unlock next set of buildings
-            elif not self.structures(unit_type["supply"]) and self.already_pending(unit_type["supply"]) == 0:
+            elif self.structures(unit_type["supply"]).amount == 0 and self.already_pending(unit_type["supply"]) == 0:
                 if self.can_afford(unit_type["supply"]):
                     await self.build(unit_type["supply"], near = command_center)
 
 
             # Build Logic: Supply Handling Past 1st Supply Depot
-            elif self.structures(unit_type["supply"]).ready.exists and self.supply_left < 4 and not self.already_pending(unit_type["supply"]):
+            elif (self.structures(unit_type["supply"]).amount >= 1 and supply_remaining <= 4) and not self.already_pending(unit_type["supply"]):
                 if self.can_afford(unit_type["supply"]):
                     target_supply = self.structures(unit_type["supply"]).closest_to(self.start_location)
                     supply_position = target_supply.position.towards(self.start_location, random.randrange(8, 15))
@@ -68,7 +75,7 @@ class JarBot(BotAI):
 
 
             if self.structures(unit_type["supply"]).ready.exists and self.can_afford(unit_type["rax"]) and not self.already_pending(unit_type['rax']):
-                if self.structures(unit_type["rax"]).amount + self.already_pending(unit_type["rax"]) > DESIRED_RAX_COUNT:
+                if self.structures(unit_type["rax"]).amount + self.already_pending(unit_type["rax"]) > desired_rax_count:
                     return
                 worker = self.worker_en_route_to_build
                 if rax_placement_posistion == None:
@@ -79,37 +86,44 @@ class JarBot(BotAI):
                 if worker and rax_placement_posistion: #Worker and Placement of Barrack was Found
                     await self.build(unit_type['rax'], near = rax_placement_posistion)
 
-            for rax in self.structures(unit_type["rax"]).ready.idle:
-                if self.supply_cap != 200: # max supply
-                    if not self.can_afford(unit_type["marine"]):
-                        break
-                    rax.train(unit_type["marine"])
+
+
+
 
         else:
             # We currently do not have any bases
             if self.can_afford(unit_type["cc"]):
                 await self.expand_now()
-
-        if self.units(unit_type["marine"]).amount >= 15:
-            if self.enemy_units:
-                for marine in self.units(unit_type["marine"]).idle:
-                    marine.attack(random.choice(self.enemy_units))
-            
-            elif self.enemy_structures:
-                for marine in self.units(unit_type["marine"]).idle:
-                    marine.attack(random.choice(self.enemy_structures))
         
         #for scv in self.workers.idle:
             #closest_min_patch = self.mineral_field.closest_to(scv)
             #scv.gather(closest_min_patch.position)
             #await self.distribute_workers()
             # closestCC = self.townhalls.ready.closest_to(scv.position)
-            # await self.do(scv.gather(self.state.mineral_field.closest_to(closestCC)))
-        
+            # await self.do(scv.gather(self.state.mineral_field.closest_to(closestCC))) 
         
         if self.alert(Alert.MineralsExhausted): # When minerals begin to run out, expand
             if self.can_afford(unit_type["cc"]):
                 await self.expand_now()
+
+
+        # attacking logic
+        if self.units(unit_type["marine"]).amount >= 30:
+            if self.enemy_units:
+                for marine in self.units(unit_type["marine"]).idle:
+                    marine.attack(random.choice(self.enemy_units))
+        
+            elif self.enemy_structures:
+                for marine in self.units(unit_type["marine"]).idle:
+                    marine.attack(random.choice(self.enemy_structures))
+
+            # otherwise attack enemy starting position
+            else:
+                for marine in self.units(unit_type["marine"]).idle:
+                    marine.attack(self.enemy_start_locations[0])
+
+        
+       
 def main():
 # Command containing Info to launch an SCII Instance with the Bot
     run_game(
